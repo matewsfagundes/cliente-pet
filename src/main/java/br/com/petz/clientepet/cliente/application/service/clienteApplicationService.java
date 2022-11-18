10ca6d7f368e212c59b1a4df0e8b54077e7c62d4
Matheus Fagundes <matewsfagundes@gmail.com>
2022-11-17 22:00:25 -0300

    implementa ClienteApplicationService e corrige clienteAPI

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/service/clienteApplicationService.java
@@ -0,0 +1,25 @@
+package br.com.petz.clientepet.cliente.application.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.petz.clientepet.cliente.application.api.ClienteRequest;
+import br.com.petz.clientepet.cliente.application.api.ClienteResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class clienteApplicationService implements clienteService {
+	private final ClienteRepository clienteRepository;
+}
+
+	@Override
+	public ClienteResponse criaCliente(ClienteRequest clienteRequest) {
+		log.info("[inica] clienteApplicationService - criaCliente");
+		Cliente cliente clienteRepository.salva(new Cliente(clienteRequest));
+		log.info("[finaliza] clienteApplicationService - criaCliente");
+		return ClienteResponse.builder()
+				.idCliente(cliente.getIdCliente()).builder;
+
+}
