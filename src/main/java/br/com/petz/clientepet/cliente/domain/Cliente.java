42a5ef15e02172d0a35af37b4a7b6617b482fd2f
Matheus Fagundes <matewsfagundes@gmail.com>
2023-08-20 12:35:10 -0300

    ajusta mapeamneto UUID

--- src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
+++ src/main/java/br/com/petz/clientepet/cliente/domain/Cliente.java
@@ -6,7 +6,10 @@ import java.util.UUID;
 
 import org.hibernate.validator.constraints.br.CPF;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -20,6 +23,8 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Cliente {
 	@Id
+	@GeneratedValue(strategy =  GenerationType.AUTO)
+	@Column(name = "id", updatable = false, unique = true, nullable = false)
 	private UUID idCliente;
     @NotBlank
 	private String momeCompleto;
