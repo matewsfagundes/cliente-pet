b6103af4a12bbc94aa76b5ee8106fd6c59d86abe
Matheus Fagundes <matewsfagundes@gmail.com>
2023-08-27 22:48:26 -0300

    implementa tratamento de erro para execao duplicado

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -3,6 +3,7 @@ package br.com.petz.clientepet.cliente.infra;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 
@@ -17,20 +18,27 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
 	private final ClienteSpringJPARepository clienteSpringJPARepository;
+
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[inicia] ClienteInfraRepository - salva");
-		clienteSpringJPARepository.save(cliente);
+		try {
+			clienteSpringJPARepository.save(cliente);
+		} catch (DataIntegrityViolationException e) {
+			throw APIException.build(HttpStatus.BAD_REQUEST, "existem dados duplicados", e);
+		}
 		log.info("[finaliza] ClienteInfraRepository - salva");
 		return cliente;
 	}
+
 	@Override
 	public List<Cliente> buscaTodosClientes() {
 		log.info("[inicia] ClienteInfraRepository - buscaTodosClientes");
-		List<Cliente>todosClientes = clienteSpringJPARepository.findAll()
-;		log.info("[finaliza] ClienteInfraRepository - buscaTodosClientes");
+		List<Cliente> todosClientes = clienteSpringJPARepository.findAll();
+		log.info("[finaliza] ClienteInfraRepository - buscaTodosClientes");
 		return todosClientes;
 	}
+
 	@Override
 	public Cliente buscaClienteAtravesId(UUID idCliente) {
 		log.info("[inicia] ClienteInfraRepository - buscaClienteAtravesId");
@@ -39,11 +47,12 @@ public class ClienteInfraRepository implements ClienteRepository {
 		log.info("[finaliza] ClienteInfraRepository - buscaClienteAtravesId");
 		return cliente;
 	}
+
 	@Override
 	public void deletaClienteAtravesId(Cliente cliente) {
 		log.info("[inicia] ClienteInfraRepository - deletaClienteAtravesId");
 		clienteSpringJPARepository.delete(cliente);
 		log.info("[finaçiza] ClienteInfraRepository - deletaClienteAtravesId");
-		
+
 	}
 }
